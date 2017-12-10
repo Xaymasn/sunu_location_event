b3e69b18bd3be0f6c5e86a10636d499a704fa416
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-12-10 11:37:42 +0000

    Correction : Modele

--- models/models.py
+++ models/models.py
@@ -4,16 +4,15 @@ from odoo import models, fields, api
 
 class hdlabs_facture_senegalbaches(models.Model):
     _inherit = "sale.order"
-    #_column = {'nb_jours': fields.Char('Nombre de jours')}
     nb_jours = fields.Char('Nombre de jours')
 
+#     _column = {'nb_jours': fields.Char('Nombre de jours')}
 #     _name = 'hdlabs_facture_senegalbaches.hdlabs_facture_senegalbaches'
 #     name = fields.Char()
 #     value = fields.Integer()
-
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+#     self.value2 = float(self.value) / 100
