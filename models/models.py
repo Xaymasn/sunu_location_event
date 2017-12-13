fb21a5966e976cd4f95c93d6e6a68d9c6ddce0f6
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-12-13 14:57:23 +0000

    Corrections sur la vatiable nombreJours

--- models/models.py
+++ models/models.py
@@ -9,9 +9,6 @@ class Sunulocationevent_order(models.Model):
     dateEvent = fields.Date("Date l'événement")
     dateMontage = fields.Date("Date de montage")
     dateDemontage = fields.Date("Date de démontage")
-
-class Sunulocationevent_orderline(models.Model):
-    _inherit = 'sale.order.line'
     nombreJours = fields.Integer("Nombre de jours")
 
 #     _name = 'hdlabs_facture_senegalbaches.hdlabs_facture_senegalbaches'
