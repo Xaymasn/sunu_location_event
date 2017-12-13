7da4b73f8cce78c569ca84d6c79d78de49579c83
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-12-13 18:24:57 +0000

    Corrections

--- models/models.py
+++ models/models.py
@@ -2,13 +2,16 @@
 
 from odoo import models, fields, api
 
-class Sunulocationevent_order(models.Model):
-    _inherit = 'sale.order.line'
-    
+class Sle_order(models.Model):
+    _inherit = 'sale.order'
     lieuEvent = fields.Char("Lieu de l'événement")
     dateEvent = fields.Date("Date l'événement")
     dateMontage = fields.Date("Date de montage")
     dateDemontage = fields.Date("Date de démontage")
+    nombreJours = fields.Integer("Nombre de jours")
+
+class Sle_orderline(models.Model):
+    _inherit = 'sale.order.line'
     nombreJours = fields.Integer("Nombre de jours")
 
     # onchange handler
