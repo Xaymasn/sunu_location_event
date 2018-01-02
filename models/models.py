fe829540f5b88e6bc878c761c97615525455642e
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-01-02 13:39:22 +0000

    Correction - Test

--- models/models.py
+++ models/models.py
@@ -14,14 +14,14 @@ class Sle_orderline(models.Model):
     _inherit = 'sale.order.line'
     nombreJours = fields.Integer(string="Nombre de jours", compute="_autoCalcNbJours")
 
-    @api.onchange('product_uom_qty','unit_price','nombreJours')
+    @api.onchange('product_uom_qty','price_unit','nombreJours')
     def _onchange_price(self):
         for record in self:
-            record.price_subtotal = record.product_uom_qty * record.nombreJours * record.unit_price
+            record.price_subtotal = record.product_uom_qty * record.nombreJours * record.price_unit
         # Can optionally return a warning and domains
 
     # Calcule automatiquement le nombre de jours de location à partir de la date de montage et de la date de démontage
     #@api.depends('dateMontage','dateDemontage')
-    #def _autoCalcNbJours(self):
-    #    for record in self:
-    #        record.nombreJours = (self.dateDemontage - self.dateMontage).days
+    def _autoCalcNbJours(self):
+        for record in self:
+            record.nombreJours = (self.dateDemontage - self.dateMontage).days
