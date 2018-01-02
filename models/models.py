2cec3d8ee19ab17ff018ce4de33ef93171d5296c
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-01-02 13:19:07 +0000

    Calcul automatique du nombre de jours et corrections de la mise en forme pour les colonnes : Rapport PDF

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import date
 
 class Sle_order(models.Model):
     _inherit = 'sale.order'
@@ -11,15 +12,16 @@ class Sle_order(models.Model):
 
 class Sle_orderline(models.Model):
     _inherit = 'sale.order.line'
-    nombreJours = fields.Integer("Nombre de jours")
+    nombreJours = fields.Integer(string="Nombre de jours", compute="_autoCalcNbJours")
 
-    @api.onchange('product_uom_qty', 'nombreJours','unit_price')
+    @api.onchange('product_uom_qty','unit_price','nombreJours')
     def _onchange_price(self):
-        self.price_subtotal = self.product_uom_qty * self.nombreJours * self.unit_price
+        for record in self:
+            record.price_subtotal = record.product_uom_qty * record.nombreJours * record.unit_price
         # Can optionally return a warning and domains
 
-
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#     self.value2 = float(self.value) / 100
+    # Calcule automatiquement le nombre de jours de location à partir de la date de montage et de la date de démontage
+    @api.depends('dateMontage','dateDemontage')
+    def _autoCalcNbJours(self):
+        for record in self:
+            record.nombreJours = (self.dateDemontage - self.dateMontage).days
