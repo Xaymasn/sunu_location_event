73ff4324a8e6ba5d2cc9e0597f87f8d77bb9cee5
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-02-15 11:57:52 +0000

    Test autocalc

--- models/models.py
+++ models/models.py
@@ -14,28 +14,7 @@ class Sle_orderline(models.Model):
     nombreJours = fields.Integer("Nombre de jours",default=1)
 
    # price_subtotal = fields.Float(compute='_amount_line', string='Subtotal')
-    """
-        Nous allons mettre à jour le prix pour qu'il soit égal au prix unitaire multiplié par le nombre de jours
-    """
-    def _amount_line(self, cr, uid, ids, field_name, arg, context=None):
-        tax_obj = self.pool.get('account.tax')
-        cur_obj = self.pool.get('res.currency')
-        res = {}
-        if context is None:
-            context = {}
-        for line in self.browse(cr, uid, ids, context=context):
-            # C'est dans cette ligne qu'on multiplie par le nombre de jours
-            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0) * nombreJours
-            taxes = tax_obj.compute_all(cr, uid, line.tax_id, price, line.product_uom_qty, line.product_id, line.order_id.partner_id)
-            cur = line.order_id.pricelist_id.currency_id
-            #This gets the delivery_price
-            total=0
-            for delivery_line in line.delivery_lines:
-                total+=delivery_line.delivery_price
-
-            res[line.id] = cur_obj.round(cr, uid, cur, taxes['total'])+total
-    return res
-
+    
 
     #self._columns['price_subtotal']._fnct = _amount_line
 
