26b0ec3bfe84a09ed403ef9afa408d33849464fd
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-16 12:22:39 +0000

    [FIX] : mise à jour des fichiers de données dans le manifest

--- __manifest__.py
+++ __manifest__.py
@@ -21,6 +21,7 @@
         'views/saleorder.form.sle.xml',
         'views/saleorder.tree.sle.xml',
         'views/saleorder.report.sle.xml',
+        'views/invoice.form.sle.xml',
         'views/invoice.report.sle.xml',
     ],
     'demo': [],
