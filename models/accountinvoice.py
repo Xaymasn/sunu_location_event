7d99010502eb2adbd3566b64d5cea4d2fc620359
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-06-19 20:32:17 +0000

    [TEST] Calcul du Sous-Total incluant le nombre de jours de location

--- models/accountinvoice.py
+++ models/accountinvoice.py
@@ -23,8 +23,8 @@ class AccountInvoiceLine(models.Model):
         currency = self.invoice_id and self.invoice_id.currency_id or None
         price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
         taxes = False
-        if self.invoice_line_tax_ids:
-            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.nombreJours, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
+        #if self.invoice_line_tax_ids:
+        #    taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.nombreJours, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
 
         # Calcul du sous-total de la ligne
         #self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price * self.nombreJours
@@ -38,11 +38,11 @@ class AccountInvoiceLine(models.Model):
         self.price_subtotal_signed = price_subtotal_signed * sign
 
 # Modification du modèle de Taxes
-'''class AccountTax(models.Model):
+class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     @api.multi
-    def compute_all(self, price_unit, currency=None, nombreJours=1, quantity=1.0, product=None, partner=None):
+    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None):
         if len(self) == 0:
             company_id = self.env.user.company_id
         else:
@@ -50,7 +50,22 @@ class AccountInvoiceLine(models.Model):
         if not currency:
             currency = company_id.currency_id
         taxes = []
+        # By default, for each tax, tax amount will first be computed
+        # and rounded at the 'Account' decimal precision for each
+        # PO/SO/invoice line and then these rounded amounts will be
+        # summed, leading to the total amount for that tax. But, if the
+        # company has tax_calculation_rounding_method = round_globally,
+        # we still follow the same method, but we use a much larger
+        # precision when we round the tax amount for each line (we use
+        # the 'Account' decimal precision + 5), and that way it's like
+        # rounding after the sum of the tax amounts of each line
         prec = currency.decimal_places
+
+        # In some cases, it is necessary to force/prevent the rounding of the tax and the total
+        # amounts. For example, in SO/PO line, we don't want to round the price unit at the
+        # precision of the currency.
+        # The context key 'round' allows to force the standard behavior.
+
         round_tax = False if company_id.tax_calculation_rounding_method == 'round_globally' else True
         round_total = True
         if 'round' in self.env.context:
@@ -59,17 +74,25 @@ class AccountInvoiceLine(models.Model):
 
         if not round_tax:
             prec += 5
-        # Modification du calcul de taxe
-        # Ce calcul engendre une erreur du type :unsupported operand type(s) for *: 'float' and 'product.product' 
-        # On le met en commentaire pour le moment
-        # total_excluded = total_included = base = round(price_unit * quantity, prec)
-        #total_excluded = total_included = base = price_unit *  quantity  * nombreJours
+
+        base_values = self.env.context.get('base_values')
+
+        if not base_values:
+            total_excluded = total_included = base = round(price_unit * quantity, prec)
+        else:
+            total_excluded, total_included, base = base_values
+
+        # Sorting key is mandatory in this case. When no key is provided, sorted() will perform a
+        # search. However, the search method is overridden in account.tax in order to add a domain
+        # depending on the context. This domain might filter out some taxes from self, e.g. in the
+        # case of group taxes.
 
         for tax in self.sorted(key=lambda r: r.sequence):
             if tax.amount_type == 'group':
-                ret = tax.children_tax_ids.compute_all(price_unit, currency, quantity, product, partner)
-                #total_excluded = ret['total_excluded']
-                base = ret['base']
+                children = tax.children_tax_ids.with_context(base_values=(total_excluded, total_included, base))
+                ret = children.compute_all(price_unit, currency, quantity, product, partner)
+                total_excluded = ret['total_excluded']
+                base = ret['base'] if tax.include_base_amount else base
                 total_included = ret['total_included']
                 tax_amount = total_included - total_excluded
                 taxes += ret['taxes']
@@ -84,29 +107,29 @@ class AccountInvoiceLine(models.Model):
             if tax.price_include:
                 total_excluded -= tax_amount
                 base -= tax_amount
+
             else:
                 total_included += tax_amount
-            
+
+            # Keep base amount used for the current tax
             tax_base = base
-            
             if tax.include_base_amount:
                 base += tax_amount
-
             taxes.append({
                 'id': tax.id,
                 'name': tax.with_context(**{'lang': partner.lang} if partner else {}).name,
                 'amount': tax_amount,
+                'base': tax_base,
                 'sequence': tax.sequence,
                 'account_id': tax.account_id.id,
                 'refund_account_id': tax.refund_account_id.id,
                 'analytic': tax.analytic,
-                'base': tax_base,
+                'price_include': tax.price_include,
             })
+
         return {
             'taxes': sorted(taxes, key=lambda k: k['sequence']),
-            'total_excluded': total_excluded,
-            'total_included': total_included,
+            'total_excluded': currency.round(total_excluded) if round_total else total_excluded,
+            'total_included': currency.round(total_included) if round_total else total_included,
             'base': base,
-    }
-    '''
-    
+        }
