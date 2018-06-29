93d0e421b5004287947dedf6012fff320297b035
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-06-29 13:19:59 +0000

    [UPDATE] : Ajout des lignes de totaux dans le rapport de devis

--- models/accountinvoice.py
+++ models/accountinvoice.py
@@ -1,13 +1,111 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
 
-class Sle_accountinvoice(models.Model):
+
+class AccountInvoice(models.Model):
     _inherit = "account.invoice"
     lieuEvent_ = fields.Char("Lieu de l'événement")
     dateEvent_ = fields.Date("Date l'événement")
     dateMontage_ = fields.Date("Date de montage")
     dateDemontage_ = fields.Date("Date de démontage")
+    # Le type de remise
+    discount_type = fields.Selection([('percent', 'Pourcentage'), ('amount', 'Montant fixe')], string='Type de remise', readonly=True, states={'draft': [('readonly', False)]}, default='percent')
+    # Le taux de remise
+    discount_rate = fields.Float('Remise', digits=(16, 2), readonly=True, states={'draft': [('readonly', False)]})
+    # Montant de la remise
+    amount_discount = fields.Monetary(string='Remise', store=True, readonly=True, compute='_compute_amount', track_visibility='always')
+    # Montant négatif de la remise (juste utilisé à des fins d'affichage)
+    amount_discount_negative = fields.Monetary(string='Remise', store=True, readonly=True, compute='_compute_amount', digits=dp.get_precision('Account'), track_visibility='always')
+    # Montant sans rabais (coût initial avant rabais)
+    amount_without_discount = fields.Monetary(string='Montant initial', store=True, readonly=True, compute='_compute_amount', digits=dp.get_precision('Account'), track_visibility='always')
+
+    @api.one
+    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding', 'currency_id', 'company_id', 'date_invoice', 'type')
+    def _compute_amount(self):
+        for line in self.invoice_line_ids:
+            # Le sous-total (HT) des articles
+            self.amount_untaxed += line.price_subtotal
+            # La remise totale des articles
+            self.amount_discount += (line.quantity * line.price_unit * line.discount) / 100
+
+        for line in self.tax_line_ids:
+            # Le montant des taxes
+            self.amount_tax += line.amount_total
+
+        self.amount_total = self.amount_untaxed + self.amount_tax
+
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if self.currency_id and self.currency_id != self.company_id.currency_id:
+            currency_id = self.currency_id.with_context(date=self.date_invoice)
+            amount_total_company_signed = currency_id.compute(self.amount_total, self.company_id.currency_id)
+            amount_untaxed_signed = currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
+        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.amount_total_company_signed = amount_total_company_signed * sign
+        self.amount_total_signed = self.amount_total * sign
+        self.amount_untaxed_signed = amount_untaxed_signed * sign
+
+        self.amount_discount_negative = (-1) * self.amount_discount
+        self.amount_without_discount = self.amount_untaxed + self.amount_discount
+
+    @api.onchange('discount_type', 'discount_rate', 'invoice_line_ids')
+    def supply_rate(self):
+        """
+        Mise à jour des taux de remise linéaire
+        :return:
+        """
+        for inv in self:
+            if inv.discount_type == 'percent':
+                for line in inv.invoice_line_ids:
+                    line.discount = inv.discount_rate
+            else:
+                total = discount = 0.0
+                for line in inv.invoice_line_ids:
+                    total += (line.quantity * line.price_unit)
+                if inv.discount_rate != 0:
+                    discount = (inv.discount_rate / total) * 100
+                else:
+                    discount = inv.discount_rate
+                for line in inv.invoice_line_ids:
+                    line.discount = discount
+
+    @api.multi
+    def compute_invoice_totals(self, company_currency, invoice_move_lines):
+        """
+        Calcul des différents totaux de la facture : Total HT/TTC de la facure, rabais, etc.
+        :param company_currency:
+        :param invoice_move_lines:
+        :return:
+        """
+        total = 0
+        total_currency = 0
+        for line in invoice_move_lines:
+            if self.currency_id != company_currency:
+                currency = self.currency_id.with_context(
+                    date=self.date or self.date_invoice or fields.Date.context_today(self))
+                line['currency_id'] = currency.id
+                line['amount_currency'] = currency.round(line['price'])
+                line['price'] = currency.compute(line['price'], company_currency)
+            else:
+                line['currency_id'] = False
+                line['amount_currency'] = False
+                line['price'] = line['price']
+            if self.type in ('out_invoice', 'in_refund'):
+                total += line['price']
+                total_currency += line['amount_currency'] or line['price']
+                line['price'] = - line['price']
+            else:
+                total -= line['price']
+                total_currency -= line['amount_currency'] or line['price']
+        return total, total_currency, invoice_move_lines
+
+    @api.multi
+    def button_dummy(self):
+        self.supply_rate()
+        return True
+
 
 # Modification des lignes de factures.
 # On veut calculer le sous-total en multipliant chaque ligne par le nombre de jours de location
@@ -15,20 +113,28 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
     # Nombre de jours de location
     nombrejours = fields.Integer("Nombre de jours",default=1,required=True)
+    # Taux de remise linéaire
+    discount = fields.Float(string='Discount (%)', digits=(16, 20), default=0.0)
 
     @api.one
     @api.depends('nombrejours', 'price_unit', 'discount', 'invoice_line_tax_ids', 'quantity', 'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id', 'invoice_id.date_invoice', 'invoice_id.date')
     def _compute_price(self):
+        """
+        Calcul des différents prix de la ligne de facture : Prix de l'article, prix de la location, rabais, montants HT/TTC de la ligne
+        :return:
+        """
         currency = self.invoice_id and self.invoice_id.currency_id or None
+        # Prix de vente d'un article
         price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-        # Prix unitaire d'un article pendant la durée totale de location:
+        # Prix de vente * 'Durée de location' d'un article
         rentalprice = self.nombrejours * price
         taxes = False
         if self.invoice_line_tax_ids:
             taxes = self.invoice_line_tax_ids.compute_all(rentalprice, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
         
-        # Calcul du sous-total de la ligne
+        # Total HT de la ligne de facture
         self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price * self.nombrejours
+        # Total TTC de la ligne de facture
         self.price_total = taxes['total_included'] if taxes else self.price_subtotal
         
         if self.invoice_id.currency_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
--- models/saleorder.py
+++ models/saleorder.py
@@ -1,14 +1,120 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
 
-class Sle_order(models.Model):
+
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
     lieuEvent = fields.Char("Lieu de l'événement")
     dateEvent = fields.Date("Date l'événement")
     dateMontage = fields.Date("Date de montage")
     dateDemontage = fields.Date("Date de démontage")
+    # Le type de remise
+    discount_type = fields.Selection(
+        [('percent', 'Pourcentage'), ('amount', 'Montant fixe')],
+        string='Type de remise',
+        readonly=True,
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, default='percent'
+    )
+    # Le taux de remise
+    discount_rate = fields.Float('Remise', digits=dp.get_precision('Account'), readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+    # Montant sans taxe
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', track_visibility='always')
+    # Montant total des taxes
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all', track_visibility='always')
+    # Montant Total
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all', track_visibility='always')
+    # Montant de la remise
+    amount_discount = fields.Monetary(string='Remise', store=True, readonly=True, compute='_amount_all', digits=dp.get_precision('Account'), track_visibility='always')
+    # Montant négatif de la remise (juste utilisé à des fins d'affichage)
+    amount_discount_negative = fields.Monetary(string='Remise', store=True, readonly=True, compute='_amount_all', digits=dp.get_precision('Account'), track_visibility='always')
+    # Montant sans rabais (coût initial avant rabais)
+    amount_without_discount = fields.Monetary(string='Montant initial', store=True, readonly=True, compute='_amount_all', digits=dp.get_precision('Account'), track_visibility='always')
 
-#class Sle_orderline(models.Model):
-#    _inherit = 'sale.order.line'
-#    nombreJours = fields.Integer("Nombre de jours",default=1)
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Calcul des totaux du devis
+        :return:
+        """
+        for order in self:
+            amount_untaxed = amount_tax = amount_discount = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+                amount_discount += (line.product_uom_qty * line.price_unit * line.nombrejours * line.discount) / 100
+
+            amount_discount_negative = (-1) * amount_discount
+
+            order.update({
+                'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
+                'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+                'amount_discount': order.pricelist_id.currency_id.round(amount_discount),
+                'amount_discount_negative': order.pricelist_id.currency_id.round(amount_discount_negative),
+                'amount_total': amount_untaxed + amount_tax,
+                'amount_without_discount': amount_untaxed + amount_discount,
+            })
+
+    # Si un des champs définis est mis à jour, on met à jour le taux de rabais
+    @api.onchange('discount_type', 'discount_rate', 'order_line')
+    def supply_rate(self):
+        """
+        Mise à jour des taux de rremise
+        :return:
+        """
+        for order in self:
+            # Si le type de remise est en pourcentage, on applique ce taux sur toutes les lignes
+            if order.discount_type == 'percent':
+                for line in order.order_line:
+                    line.discount = order.discount_rate
+            # Sinon, on utilise le montant
+            else:
+                total = discount = 0.0
+                for line in order.order_line:
+                    total += round((line.product_uom_qty * line.price_unit * line.nombrejours))
+                # Ici on calcule la valeur en % (pourcent) du montant fixe de la remise. Parceque Odoo utilise des valeurs en % pour calculer les remises
+                if order.discount_rate != 0:
+                    discount = (order.discount_rate / total) * 100
+                else:
+                    discount = order.discount_rate
+                for line in order.order_line:
+                    line.discount = discount
+
+    @api.multi
+    def _prepare_invoice(self, ):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals.update({
+            'discount_type': self.discount_type,
+            'discount_rate': self.discount_rate
+        })
+        return invoice_vals
+
+    @api.multi
+    def button_dummy(self):
+        self.supply_rate()
+        return True
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    nombrejours = fields.Integer("Nombre de jours", default=1, required=True)
+    discount = fields.Float(string='Remise (%)', digits=(16, 20), default=0.0)
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Calcule et met à jour le montant de chaque ligne de devis
+        - 'price_tax' : Le total des taxes de la ligne
+        - 'price_total' : Le total TTC de la ligne
+        - 'price_subtotal' : Le total HT de la ligne
+        """
+        for line in self:
+            # On multiplie le prix initial par le nombre de jours
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0) * line.nombrejours
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
